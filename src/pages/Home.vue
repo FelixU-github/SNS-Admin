12007e6ed0b90dbaa33b6645f93405b697c79987
felixyuu <128110495+FelixU-github@users.noreply.github.com>
2024-07-31 16:21:47 +0800

    homepage update
     更新了主页，以及所需要的表格组件

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,35 @@
 <template>
+  <div class="charts-container">
+    <div class="lineChart">
+    <LineChart />
+  </div>
+
+  </div>
+
   <div>
+    <MyChart />
     
-    <Echart />
   </div>
   
 </template>
 
-<script>
-import Echart from "../componments/Echart.vue";
-export default {
-  name: 'YourComponentName', // 给你的组件一个名称
-  components: {
-    Echart
-  }
-}
+<script setup>
+import LineChart from "@/componments/LineChart.vue";
+import MyChart from "@/componments/MyChart.vue";
+
 
 </script>
+
+<style scoped>
+.charts-container {
+  display: flex;
+  justify-content: space-between; /* 控制图表之间的间距 */
+  gap: 20px; /* 控制图表之间的间距 */
+}
+
+.lineChart {
+  flex: 1; /* 让图表组件平分父容器的宽度 */
+  min-width: 300px; /* 控制最小宽度，防止过度收缩 */
+  height: 400px; /* 控制图表高度 */
+}
+</style>
